25ce71e1bb62b9004dcdcbc18c6ccd578e806460
Ankitkumargautam <ankitogn4@gmail.com>
2024-10-25 21:46:58 +0530

    integrated material ui

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+import Button from "@mui/material/Button";
+
+const App = () =>  {
+  return (
+    <div className="App_Base">
+      Hello ther TRRE is good lokking theire aldbl gabbage.
+      <div>
+        <Button variant="contained">Hello world</Button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
